a71c4444a8efd4eef201ad85872dea30536d6f37
Kriviit <kriviitbigdata@gmail.com>
2025-08-05 20:18:13 +0530

    completed loops

---
+++ loops/for.py
@@ -0,0 +1,4 @@
+arr=[11,12,13,14,15,16,17,18]#collection ,list, ds
+
+for i in arr:
+    print(i)
---
+++ loops/while.py
@@ -0,0 +1,7 @@
+i=1
+j=1
+print("starting of the loop")
+while(i<=20):# T T t ........
+    print(j) # 1 1 ..........
+    j+=1 # 1 2 3 4 5 6 7 8 9 
+print("ending of the loop")
